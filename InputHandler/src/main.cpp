c9ae5f8facecee640afc7eaf2fba7c1bb317a35a
pepema <petter.emanuelsson@gmail.com>
2020-11-04 14:07:26 +0100

    beautification

--- InputHandler/src/main.cpp
+++ InputHandler/src/main.cpp
@@ -4,8 +4,6 @@
 #include "input_handler.hpp"
 #include "can_encoder.hpp"
 
-// https://github.com/siposcsaba89/socketcan-cpp
-
 int main(){
   CanWriter& writer = CanWriter::getInstance();
   CanEncoder encoder;
@@ -13,11 +11,9 @@ int main(){
   uint8_t data [8] = {0,0,0,0,0,0,0,0};
   int key_pressed;
 
-  while (true) {
+  while (true && key_pressed != 27) {
     key_pressed = keypresshandler.GetPressedKey();
     if(key_pressed != -1)
-      if(key_pressed == 27)
-        break;
       encoder.Encode(key_pressed,data);
     writer.SendFrame(1, data);
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
