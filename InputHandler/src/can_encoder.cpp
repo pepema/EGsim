9f09de98fcacbc449b7ead4b14613d1d48546682
pepema <petter.emanuelsson@gmail.com>
2020-11-05 13:17:50 +0100

    updated from hex to dec for accpdl

--- InputHandler/src/can_encoder.cpp
+++ InputHandler/src/can_encoder.cpp
@@ -6,9 +6,9 @@ void CanEncoder::Encode(int input, uint8_t* data){
   } else if(input == 's'){
     data[7] = 0;
   } else if(input == 'a'){
-    if (data[6]>0) data[6]-=0x10;
+    if (data[6]>0) data[6]-=10;
   } else if(input == 'A'){
-    if(data[6]<0xA0) data[6]+=0x10;
+    if(data[6]<100) data[6]+=10;
   } else {
     //std::cout << "this key was pressed: " << ch << std::endl;
   }
