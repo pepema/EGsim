e41bfb4f2fc5c9928619b46cf2da21f9a2ee63a8
pepema <petter.emanuelsson@gmail.com>
2020-11-06 11:28:31 +0100

    now we have a break pedal

--- InputHandler/src/can_encoder.cpp
+++ InputHandler/src/can_encoder.cpp
@@ -15,5 +15,9 @@ void CanEncoder::Encode(int input, uint8_t* data){
     data[5] = 0;
   } else if(input == 'r'){
     data[5] = 2;
+  } else if(input == 'B'){
+    if (data[4]< 100) data[4] += 10;
+  } else if(input  == 'b'){
+    if (data[4]> 0) data[4] -= 10;
   }
 }
