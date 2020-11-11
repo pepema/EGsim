cf578065bf52e2743d363d82c94c980572d14095
viral <viral.soni@volvocars.com>
2020-11-11 11:08:58 +0100

    Removed comments from main

--- EngineSimulator/src/main.cpp
+++ EngineSimulator/src/main.cpp
@@ -1,12 +1,12 @@
+#include <thread>
+#include <iomanip>
+#include <future>
 #include "signal_decoder.h"
 #include "can_encoder.h"
 #include "can_reader_writer.hpp"
-#include <thread>
-#include <iomanip>
 #include "engine.h"
 #include "gearbox.h"
-#include <future>
-#include <atomic>
+
 
 void readUpdate(CanReaderWriter& can_r_w, std::promise<bool>* app_start)
 {
@@ -19,17 +19,10 @@ void readUpdate(CanReaderWriter& can_r_w, std::promise<bool>* app_start)
     }
 }
 
-/*void readUpdate(CanReaderWriter& can_r_w)
-{
-    ;
-}
-*/
-
 void processWrite(CanReaderWriter& can_r_w, bool app_start)
 {
     SignalDecoder my_signal_decoder;
     CANEncoder my_encoder;
-    //CanReaderWriter my_reader_writer;
     FrameData output_data;
     Engine my_engine;
     Gearbox my_gearbox;
@@ -38,15 +31,12 @@ void processWrite(CanReaderWriter& can_r_w, bool app_start)
     while(app_start)
     {
         my_signal_decoder.setIpFrame(can_r_w.getData());
-        //Set Values Tran
+
         my_engine.setHazard(my_signal_decoder.getHazard());
-        //my_engine.setHazard(true);
+
         if(my_engine.getHazard() == true)
         {
-            //std::cout<<"Failure!!!"<<std::endl<<"Bad coding"<<std::endl<<std::endl;
-            //std::this_thread::sleep_for(std::chrono::milliseconds(500));
             app_start = false;
-            //break;
         }
         my_engine.setEngineStatus(my_signal_decoder.getEngineStatus());
         my_gearbox.updateGear(my_signal_decoder.getGearinput(), my_signal_decoder.getBrakeinput());
@@ -75,9 +65,6 @@ void processWrite(CanReaderWriter& can_r_w, bool app_start)
                   << " Brake: "               << std::setfill(' ') << std::setw(3) << static_cast<int>(my_signal_decoder.getBrakeinput()) << "%"
                   << " Acceleration: "        << std::setfill(' ') << std::setw(3) << static_cast<int>(my_signal_decoder.getAcceleration()) << "%"
                   << '\r' << std::flush;
-
-        //std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        //std::cout<<std::endl;
     }
 }
 
@@ -85,35 +72,14 @@ int main()
 {
     bool app_start = true;
     CanReaderWriter my_reader_writer;
-    std::promise<bool> start_cond_ru_p; //promise for readUpdate
-    //std::atomic_bool my_flag{true};
-    //std::promise<bool> start_cond_pw_p;  //promise for processWrite
-    //std::future<bool> start_cond_f = start_cond_p.get_future();
-
-    //std:condition_variable cv;
-
-    //start_cond_ru_p.set_value(true);
-    //start_cond_pw_p.set_value(true);
+    std::promise<bool> start_cond_ru_p; 
 
     std::thread read_thread (readUpdate, std::ref(my_reader_writer), &start_cond_ru_p);
-    //std::thread read_thread (readUpdate, std::ref(my_reader_writer));
-
-    //app_start = false;
-    //appstart =false;
-    
-    //std::thread process_thread (processWrite, my_reader_writer, start_cond_pw_p);
+ 
     processWrite(my_reader_writer,app_start);
 
-    //app_start = false in thread 1
-    //start_cond_pw_p.set_value(false);
-    
-    //The below statements will NEVER be called!
-
-    //start_cond_ru_p.set_value(false);
     start_cond_ru_p.set_value(false);
-    //my_flag = false;
     read_thread.join();
-    //read_thread.detach();
 
     return 0;
 }
