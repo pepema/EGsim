c36e447128c1ddbf30e5f9102016141eaef748d2
viral <viral.soni@volvocars.com>
2020-11-11 10:44:27 +0100

    Changed constructor and setIpFrame

--- EngineSimulator/src/signal_decoder.cpp
+++ EngineSimulator/src/signal_decoder.cpp
@@ -1,7 +1,11 @@
 #include "signal_decoder.h" 
 
-SignalDecoder::SignalDecoder(/* args */)
+SignalDecoder::SignalDecoder()
 { 
+    for(auto i = 0 ; i < 8 ; i++)
+    {
+        frame_data_ip.data[i] = 0;
+    }
 }
 
 SignalDecoder::~SignalDecoder()
@@ -13,19 +17,9 @@ uint8_t SignalDecoder::getAcceleration()
     return frame_data_ip.data[6];
 }
 
-/*void SignalDecoder::setFrameData(const uint8_t (&frameDataVal)[8])
-{
-    for (size_t i = 0; i < 8; i++)
-    {
-        frameDataIp.data[i] = frameDataVal[i];
-    }
-}*/
 void SignalDecoder::setIpFrame(const FrameData & frame_data_val)
 {
-    for (size_t i = 0; i < 8; i++)
-    {
-        frame_data_ip.data[i] = frame_data_val.data[i];
-    }
+    frame_data_ip = frame_data_val;
 }
 
 bool SignalDecoder::getEngineStatus()
@@ -33,11 +27,9 @@ bool SignalDecoder::getEngineStatus()
     if(frame_data_ip.data[7] == 0)
     {
         return false;
-        //frame_data_op.data[3] = 0;
     }
     else
     {
-        //frame_data_op.data[3] = 1;
         return true;
     }
 }
@@ -55,30 +47,9 @@ bool SignalDecoder::getHazard()
     if(frame_data_ip.data[0] == 0)
     {
         return false;
-        //frame_data_op.data[3] = 0;
     }
     else
     {
-        //frame_data_op.data[3] = 1;
         return true;
     }
 }
-/*
-void SignalDecoder::updateFrameData()
-{
-    CANReader *myReader = &CANReader::getInstance();
-    frameDataIp = myReader->getData();
-}
-
-frameData SignalDecoder::getData()
-{
-    return frameDataOp;
-}
-
-
-getData()
-{
-    CANReader myReader;
-    frameDataIp.data = myReader.getData();
-}
-*/
