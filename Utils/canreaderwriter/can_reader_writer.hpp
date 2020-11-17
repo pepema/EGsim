7ba92d0a13ef9671d4d6f524f503cd49c15ae225
viral <viral.soni@volvocars.com>
2020-11-17 08:58:13 +0100

    Correct sequence for input frame structure

--- Utils/canreaderwriter/can_reader_writer.hpp
+++ Utils/canreaderwriter/can_reader_writer.hpp
@@ -28,16 +28,12 @@ typedef union FrameData{
     }input;
     struct __attribute__((packed)) op_bits{
     unsigned int hazard:1;
-    unsigned int RESERVED_PADDING_1:7+;
-    unsigned int accel:8;
+    unsigned int RESERVED_PADDING_1:39;
     unsigned int brake:8;
+    unsigned int accel:8;
     unsigned int gear:1;
-    unsigned int gear_number:1;
-    unsigned int RESERVED_PADDING_2:6;
-    unsigned int hazard:1;
-    unsigned int RESERVED_PADDING_3:7;
+    unsigned int RESERVED_PADDING_2:7;
     //
-    unsigned int RESERVED_PADDING_4:24;
     }output;
 
     uint8_t data[8];
