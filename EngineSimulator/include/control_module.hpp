cf002df7f46deff471b097457d0a2a06f407929b
pepema <petter.emanuelsson@gmail.com>
2020-11-19 13:06:01 +0100

    got renamed version of header files..

--- EngineSimulator/include/control_module.hpp
+++ EngineSimulator/include/control_module.hpp
@@ -1,30 +1,24 @@
-#ifndef CONTROL_MODULE_H
-#define CONTROL_MODULE_H
+#ifndef CONTROL_MODULE_HPP
+#define CONTROL_MODULE_HPP
 #include <thread>
 #include <iomanip>
 #include <future>
-#include "signal_decoder.h"
-#include "can_encoder.h"
+#include "signals.hpp"
+#include "can_encoder.hpp"
 #include "can_reader_writer.hpp"
-#include "engine.h"
-#include "gearbox.h"
+#include "engine.hpp"
+#include "gearbox.hpp"
 
 
 class ControlModule{
     public:
     void Run(DataBuffer &);
     void PowertrainControl();
-    void EvaluateEngineStatus();
     void CalculateGear();
     void ShiftGear();
-    bool EvaluateHazard();
     void Encode();
-    void SetOutputFrame();
     void SetGearMode();
-    void SendCANFrame();
     void DummyDim();
-    //inline void DecodeInputCan(DataBuffer& input_frame_buffer){signal_decoder.setIpFrame(can_r_w->getData());}
-    void DecodeInputCan(DataBuffer& input_frame_buffer);
 
     ControlModule(CanReaderWriter*);
     
@@ -32,7 +26,7 @@ class ControlModule{
     bool shift_up, shift_down;
     Engine engine;
     Gearbox gearbox;
-    SignalDecoder signal_decoder;
+    Signals signals;
     CANEncoder encoder;
     FrameData output_data;
     CanReaderWriter* can_r_w;
--- EngineSimulator/include/engine.hpp
+++ EngineSimulator/include/engine.hpp
@@ -1,8 +1,7 @@
-#ifndef ENGINE_H
-#define ENGINE_H
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
 #include <iostream>
-#include "gearbox.h"
-#include "fuel_calculator.hpp"
+#include "gearbox.hpp"
 using namespace std;
 
 namespace engparams{
@@ -29,20 +28,16 @@ private:
     bool hazard;
     void updateARPM_N();
     void updateARPM_D_R(const uint8_t& brake);
-
-    int fuel_level = 100000000, fuel_used_last_cycle=0, fuel_age=0;
 public:
     void setEngineStatus(const bool& ES);
     void updateTRPM(const uint8_t& accel);
     void updateARPM(const uint8_t& brake, const GearMode& gear);
+
     bool getEngineStatus();
+
     uint16_t getARPM();
     void setHazard(const bool& hazard);
     bool getHazard();
-    inline int getFuelLevel() {return fuel_level/1000000;}
-    inline int GetFuelUsed() {return fuel_used_last_cycle;}
-    inline int GetFuelAge() {return fuel_age;}
-    FuelCalculator* fuel_calculator;
 
     Engine();
  
