cce76f505086c7d437c76698cfed0d925fb47c6b
wstridsberg <stridsbergw@gmail.com>
2020-11-19 14:43:50 +0100

    continued Gearbox test

--- EngineSimulator/testGearbox/gearbox_test.cpp
+++ EngineSimulator/testGearbox/gearbox_test.cpp
@@ -2,6 +2,19 @@
 #include "engine.h"
 #include "gearbox.h"
 
+
+class GearboxTest : public ::testing::Test{
+    protected:
+        Gearbox gearbox_init;
+    void SetUp() override{
+        
+
+    }
+
+
+}
+
+
 TEST (GearboxTest, initGearMode){
     Gearbox gearbox;
     EXPECT_EQ(GearMode::N, gearbox.getGearMode());
